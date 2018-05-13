9c8be7e44226c0c5a051d3588c269c8b4c7089fc
Alex Whaley <alex.whaley.1994@gmail.com>
2018-05-13 17:56:36 +0100

    Initial file and code setup

---
+++ Assets/Scripts/RPG/Characters/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    private int _health = 500;
+
+    public bool IsAlive { get; private set; }
+
+    public void DoDamage(int damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            IsAlive = false;
+            _health = 0;
+        }
+    }
+}
